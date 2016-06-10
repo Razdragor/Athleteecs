672cdc5ad2a320873d222f701a3fb45e3b57a53f
Bertrand Jaunet <bertrand.jaunet@gmail.com>
2016-06-10 14:11:01 +0200

    footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -163,7 +163,7 @@
                 </div>
                 <div class="row copyright">
                     <div class="col-sm-12">
-                        <p>2016
+                        <p>2016 - 
                             <a href="http://localhost" target="_blank">Freeride</a>
                         </p>
                     </div>
