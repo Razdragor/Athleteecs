9df70f243b22cfd2573f6b3d2b63db6c21803207
Bertrand Jaunet <bertrand.jaunet@gmail.com>
2016-06-27 10:46:29 +0200

    ajout de la table users_demands

---
+++ database/migrations/2016_06_27_084303_create_users_demands.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersDemands extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_demands', function (Blueprint $table) {
+            $table->bigInteger('user_id');
+            $table->bigInteger('userL_id');
+            $table->boolean('demands');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users_demands');
+    }
+}
