d85cb8ddb976ec0fe211976be1a897bb427c859e
Bertrand Jaunet <bertrand.jaunet@gmail.com>
2016-07-01 12:17:06 +0200

    modif

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -426,5 +426,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>-->
     @yield('js')
     {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
+
 </body>
 </html>
