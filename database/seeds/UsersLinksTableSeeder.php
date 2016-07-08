ebc3a8b3e84d52bbc713d5c67417ce7da89ed6dc
fakebounce <benjamin.romanelli@neuf.fr>
2016-07-09 01:13:21 +0200

    Seed user_links corrigés

--- database/seeds/UsersLinksTableSeeder.php
+++ database/seeds/UsersLinksTableSeeder.php
@@ -24,49 +24,7 @@ class UsersLinksTableSeeder extends Seeder
             'created_at' => Carbon\Carbon::now(),
             'updated_at' => Carbon\Carbon::now()
         ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '1',
-            'userL_id' => '4',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '1',
-            'userL_id' => '5',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '1',
-            'userL_id' => '6',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '1',
-            'userL_id' => '7',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '1',
-            'userL_id' => '8',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '1',
-            'userL_id' => '9',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
+        
         DB::table('users_links')->insert([
             'user_id' => '2',
             'userL_id' => '1',
@@ -76,48 +34,6 @@ class UsersLinksTableSeeder extends Seeder
 
         DB::table('users_links')->insert([
             'user_id' => '3',
-            'userL_id' => '1',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '4',
-            'userL_id' => '1',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '5',
-            'userL_id' => '1',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '6',
-            'userL_id' => '1',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '7',
-            'userL_id' => '1',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '8',
-            'userL_id' => '1',
-            'created_at' => Carbon\Carbon::now(),
-            'updated_at' => Carbon\Carbon::now()
-        ]);
-
-        DB::table('users_links')->insert([
-            'user_id' => '9',
             'userL_id' => '1',
             'created_at' => Carbon\Carbon::now(),
             'updated_at' => Carbon\Carbon::now()
