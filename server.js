9bb7d0f529592986eb0176a80f6f1f11a79b4c64
Bertrand Jaunet <bertrand.jaunet@gmail.com>
2016-07-16 03:58:37 +0200

    server.js clean

--- server.js
+++ server.js
@@ -2,36 +2,33 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var redis = require('redis');
- 
+
 server.listen(8890);
 io.on('connection', function (socket) {
- 
+
   console.log("new client connected");
-  var redisClient = redis.createClient(8890, 'athleteec.razdragor.fr');
-  redisClient.on('error',function(e){
-    console.log('error:' + e);
-  });
+  var redisClient = redis.createClient();
   redisClient.subscribe('message');
   redisClient.subscribe('change_name');
   redisClient.subscribe('add_user');
- 
+
   redisClient.on("message", function(channel, message) {
     console.log("mew message in queue "+ message + "channel");
     socket.emit(channel, message);
   });
-  
+
   redisClient.on("change_name", function(channel, message) {
     console.log("New conv name in queue "+ message + "channel");
     socket.emit(channel, message);
   });
-  
+
   redisClient.on("add_user", function(channel, message) {
     console.log("New user in queue"+ message + "channel");
     socket.emit(channel, message);
   });
- 
+
   socket.on('disconnect', function() {
     redisClient.quit();
   });
- 
+
 });
