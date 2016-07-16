3ab22baadf1d334e6f1b971d521ac37e6c7b26d4
Bertrand Jaunet <bertrand.jaunet@gmail.com>
2016-07-16 23:48:26 +0200

    modif demandsto qui déconnait

--- resources/views/front/user/show.blade.php
+++ resources/views/front/user/show.blade.php
@@ -29,13 +29,12 @@
                             <div>
                                 <a class="btn btn-block btn-success"><i class="fa fa-envelope-alt"></i>Envoyer un message</a>
                             </div>
-
                             @if(Auth::user()->isfriend(Auth::user()->id,$user->id)==='demandsfrom')
                                 <a href="{{ route('front.friends.cancel', ['friend' => $user->id]) }}" class="btn btn-block btn-success">Annuler la demande</a>
 
                             @elseif(Auth::user()->isfriend(Auth::user()->id,$user->id)==='demandsto')
                                 <a href="{{ route('front.friends.accept', ['friend' => $user->id]) }}" class="btn btn-block btn-success">Accepter la demande</a>
-                                <a href="{{ route('front.friends.cancel', ['friend' => $user->id]) }}" class="btn btn-block btn-success">Refuser la demande</a>
+                                <a href="{{ route('front.friends.cancel', ['friend' =>$user->id]) }}" class="btn btn-block btn-success">Refuser la demande</a>
 
                             @elseif(Auth::user()->isfriend(Auth::user()->id,$user->id)==='estami')
                                 <a href="{{ route('front.friends.destroy', ['friend' => $user->id]) }}" class="btn btn-block btn-success">Retirer de la liste d'amis</a>
