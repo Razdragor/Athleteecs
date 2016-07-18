08000300d14adc8c50b58de4edda437d30fe1eb9
Julian <julian.michau@gmail.com>
2016-07-18 23:28:36 +0200

    social x2

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -148,8 +148,8 @@ $user = Auth::user();
                         <a href="{{ route('user.show',['user' => Auth::user()->id]) }}"><img src="{{ Auth::user()->picture}}" alt="Avatar" class="dropdown nav-notifications img-navbarre"></a>
 
                         <li class="dropdown nav-notifications">
-                            <a href="/conversation" data-delay="0" class="dropdown-toggle">
-                                <i class="fa fa-comments-o"></i>
+                            <a href="/conversation" data-delay="0" class="dropdown-toggle" style="margin-top: 0;padding-top: 5px;">
+                                <i class="fa fa-comments-o fa-2x"></i>
                             </a>
                         </li>
 
