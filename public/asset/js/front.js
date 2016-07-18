c2a8f0c5845efe1bca555793649aef2060b0b562
Julian <julian.michau@gmail.com>
2016-07-18 22:00:46 +0200

    js

--- public/asset/js/front.js
+++ public/asset/js/front.js
@@ -178,7 +178,7 @@ $(document).ready(function() {
                                 "<div class='section-portfolio-item'>" +
                                     "<div class='picture-cadre'>"+
                                         "<div class='picture-box'>"+
-                                            "<img src='images/users/"+$picture+"' alt='image'>"+
+                                            "<img src='http://athleteec.razdragor.fr/public/images/users/"+$picture+"' alt='image'>"+
                                         "</div>"+
                                     "</div>"+
                                 "</div>"+
