d6c4e2d9a26a5938d45ae3582979d114cc73d4ad
Julian <julian.michau@gmail.com>
2016-09-01 15:48:05 +0200

    ddd

--- resources/views/admin/product/index.blade.php
+++ resources/views/admin/product/index.blade.php
@@ -42,7 +42,7 @@
                         </tr>
                         </thead>
                         <tbody>
-                        @foreach($products as $product)
+                        @foreach($products->where('active',1) as $product)
                             <tr>
                                 <td>{{ $product->id }}</td>
                                 <td><img src="{{ asset($product->picture) }}" alt="{{ $product->name }}" class="avatar" width="25" height="25"></td>
@@ -85,7 +85,7 @@
                             </tr>
                             </thead>
                             <tbody>
-                            @foreach($products as $product)
+                            @foreach($products->where('active',0) as $product)
                                 <tr>
                                     <td>{{ $product->id }}</td>
                                     <td><img src="{{ asset($product->picture) }}" alt="{{ $product->name }}" class="avatar" width="25" height="25"></td>
